5fe2aa178da0d627df950e392304720ba68ce1f0
themestermim <themestermim@gmail.com>
2024-04-17 16:12:11 +0330

    skeleton bug fixed.

--- components/landing/skeleton.vue
+++ components/landing/skeleton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-for="(company, index) in 16" :key="index" v-if="!companiesLoaded"
+    <div v-for="(company, index) in 16" :key="index" v-if="status"
         class="rounded-lg bg-white border border-[#e5e5e5] py-4 px-2 flex flex-col items-center gap-4">
         <div class="rounded-full overflow-hidden size-24 bg_skeleton border border-[#e5e5e5]"></div>
         <p class="bg_skeleton h-5 md:h-6 w-24 rounded-full mx-auto"></p>
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+import { ref, watch } from 'vue';
+
 export default {
     name: 'Skeleton',
     props: {
