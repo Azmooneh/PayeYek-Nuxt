93b51beea9097ee16d53172a1159d8220d6f583b
themestermim <themestermim@gmail.com>
2024-04-17 16:22:32 +0330

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,8 @@ export default defineNuxtConfig({
 
   srcDir: "./",
 
+  ssr: true,
+
   runtimeConfig: {
     apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
     public: {
