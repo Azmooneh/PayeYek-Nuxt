61b26e45a07ab9fec2640be784bccf3c7bd9d1d4
mehrangit <mehran.mj9999@gmail.com>
2024-04-18 10:23:54 +0330

    video bugs fixed.

--- components/companyLanding/videos/index.vue
+++ components/companyLanding/videos/index.vue
@@ -12,8 +12,8 @@
         </div>
 
         <ul
-            :class="'flex list-none lg:w-full ' + videoList.length == 1 ? 'justify-center' : (videoList.length == 2 ? 'gap-4 sm:flex-row sm:items-center flex-col lg:grid lg:grid-cols-2 overflow-auto lg:overflow-visible' : 'gap-4 sm:flex-row sm:items-center flex-col lg:grid lg:grid-cols-3 overflow-auto lg:overflow-visible')">
-            <li v-for="(video, index) in videoList.slice(0, 3)" @click="showVideoByThumbnail(video.link)" :key="index" :class="'flex-none flex flex-col ' + videoList.length == 1 ? 'w-full sm:w-[448px] md:w-[480px] lg:w-[558px] xl:w-[720px]' : 'w-full sm:w-[448px] md:w-[480px] lg:w-full'">
+            :class="'flex list-none lg:w-full ' + (videoList.length == 1 ? 'justify-center' : (videoList.length == 2 ? 'gap-4 sm:flex-row sm:items-center flex-col lg:grid lg:grid-cols-2 overflow-auto lg:overflow-visible' : 'gap-4 sm:flex-row sm:items-center flex-col lg:grid lg:grid-cols-3 overflow-auto lg:overflow-visible'))">
+            <li v-for="(video, index) in videoList.slice(0, 3)" @click="showVideoByThumbnail(video.link)" :key="index" :class="'flex-none flex flex-col ' + (videoList.length == 1 ? 'w-full sm:w-[448px] md:w-[480px] lg:w-[558px] xl:w-[720px]' : 'w-full sm:w-[448px] md:w-[480px] lg:w-full')">
                 <div class="relative w-full pt-[62%] mb-4 cursor-pointer rounded-custom overflow-hidden videoThumbnails">
                     <img :src="video.image" :alt="video.alt" class="absolute top-0 left-0 w-full h-full object-cover z-[1]" />
                     <PlayIcon classNames="size-14 sm:w-20 sm:h-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[3]" />
