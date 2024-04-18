bf1b7f396baa2fb57e0c705dc69edb4db7cf56d9
mehrangit <mehran.mj9999@gmail.com>
2024-04-18 23:04:38 +0330

    test failed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
 
   srcDir: "./",
 
-  ssr: false,
+  ssr: true,
 
   runtimeConfig: {
     apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
