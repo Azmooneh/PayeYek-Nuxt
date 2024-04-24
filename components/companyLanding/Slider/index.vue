f77b054508d3c3462e42ce2173909f5222852283
themestermim <themestermim@gmail.com>
2024-04-24 15:21:04 +0330

    testing image slider.

--- components/companyLanding/Slider/index.vue
+++ components/companyLanding/Slider/index.vue
@@ -2,22 +2,7 @@
     <section class="container mb-6 lg:mb-8" v-if="slides && slides.length > 0">
         <Swiper v-bind="settings" :modules="[SwiperPagination, SwiperAutoplay, SwiperNavigation]" :class="'land_slider slider_type_1 ' + sliderStyle">
             <SwiperSlide v-for="(slide, index) in slides" :key="index">
-                <NuxtLink :to="slide.link" class="relative w-full pt-[56%] block rounded-b-custom overflow-hidden">
-                    <img class="absolute top-0 left-0 object-cover w-full h-full" width="100%" height="100%" :src="slide.image" :alt="slide.alt">
-                    <ul v-if="slide.infos"
-                        class="list-disc list-inside absolute top-2.5 sm:top-4 sm:right-2 right-1 py-1 px-2 sm:px-4 sm:py-2 space-y-1.5 z-[1] bg-white/80 text-xs font-medium text-stone-700 rounded-custom">
-                        <template v-for="(li, innerIndex) in slide.infos" :key="innerIndex">
-                            <li class="marker:text-stone-700 sm:text-sm md:text-base" v-if="li"
-                                :class="{ 'hidden': innerIndex > 0, 'sm:list-item': innerIndex >= 1 && innerIndex < 3, 'md:list-item': innerIndex >= 3 }">
-                                <p class="inline-flex truncate"> {{ li }} </p>
-                            </li>
-                        </template>
-                    </ul>
-                    <!-- gradient background -->
-                    <div
-                        class="absolute bottom-0 left-0 w-full h-12 custom_gradient sm:h-24 lg:h-36 xl:h-52 bg-gradient-to-t from-black/80 sm:from-black/90 to-transparent">
-                    </div>
-                </NuxtLink>
+                <Slide :slide="slide" />
             </SwiperSlide>
         </Swiper>
     </section>
@@ -25,10 +10,14 @@
 
 <script>
 import { useCompanyData, useCommon } from '~/store/index';
-import { NuxtLink } from "#components";
+import Slide from './children/slide.vue';
+
 
 export default {
     name: 'Slider',
+    components: {
+        Slide,
+    },
     setup() {
         const settings = {
             slidesPerView: 1,
@@ -45,7 +34,6 @@ export default {
             },
         };
         const companyStore = useCompanyData();
-        // const styleStore = useStyles();
         const layoutStore = useCommon();
         const slides = ref(companyStore.slides);
         const sliderStyle = ref("");
