5e55fe5e13211c58ebd0f6afe0f95dfc88c74c74
themestermim <themestermim@gmail.com>
2024-04-29 11:50:20 +0330

    product type 1 added.

--- components/companyLanding/products/children/index.vue
+++ components/companyLanding/products/children/index.vue
@@ -1,11 +1,13 @@
 <template>
-    <Product_type_1 :slug="slug" v-if="productCardType == 1" />
+    <Product_type_1 :slug="slug" v-if="productCardType == 1" :borderStyle="borderStyle" />
     <Product_type_11 :slug="slug" v-if="productCardType == 11" />
 </template>
 
 <script>
 import Product_type_1 from './children/product_type_1.vue';
 import Product_type_11 from './children/product_type_11.vue';
+import {ref} from "vue";
+import {useCommon} from "~/store/index.js";
 
 export default {
     name: 'Product Types',
@@ -16,6 +18,50 @@ export default {
     props: {
         slug: String,
         productCardType: Number,
+    },
+    setup(){
+        const layoutStore = useCommon();
+        const borderStyle = ref("");
+
+        switch (layoutStore.footerData.styles.border_type) {
+            case 0:
+                switch (layoutStore.footerData.styles.product_card_type) {
+                    case 11:
+                        borderStyle.value = "";
+                        break;
+
+                    default:
+                        break;
+                }
+                break;
+            case 1:
+                switch (layoutStore.footerData.styles.product_card_type) {
+                    case 11:
+                        borderStyle.value = "border border-stone-400";
+                        break;
+
+                    default:
+                        break;
+                }
+                break;
+            case 2:
+                switch (layoutStore.footerData.styles.product_card_type) {
+                    case 11:
+                        borderStyle.value = "drop-shadow-base";
+                        break;
+
+                    default:
+                        break;
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        return {
+            borderStyle,
+        }
     }
 }
 </script>
---
+++ pages/l/[id]/v/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="">videos</div>
+</template>
+
+<script>
+export default {
+    name: 'Videos',
+}
+</script>
