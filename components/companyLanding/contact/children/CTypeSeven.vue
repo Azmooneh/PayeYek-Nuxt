aae86be220e8cbf123a2d7e3ec5bbd511d3f0d51
themestermim <themestermim@gmail.com>
2024-05-01 13:06:56 +0330

    bug fix

--- components/companyLanding/contact/children/CTypeSeven.vue
+++ components/companyLanding/contact/children/CTypeSeven.vue
@@ -4,7 +4,7 @@
             <p class="text-base sm:text-lg lg:text-xl xl:text-2xl font-normal text-white mb-8 lg:mb-12 text-center"> جهت ارتباط و اطلاع از شرایط فروش شماره خود را وارد کنید </p>
             <!-- form-->
             <form class="max-w-64 w-full flex flex-col gap-10 mx-auto items-center">
-                <input name="phone" type="tel" v-model="phone" autocomplete="off" class="border border-[#cfd1d4] focus:border-[#cfd1d4] h-12 focus:ring-0 rounded-custom outline-none w-full placeholder:text-[#888b93] text-base bg-white/20 font-normal pr-6 pl-3 text-white" placeholder="شماره همراه" value="09" />
+                <input name="phone" type="tel" v-model="phone" autocomplete="off" class="border border-[#cfd1d4] focus:border-[#cfd1d4] h-12 focus:ring-0 rounded-custom outline-none w-full placeholder:text-[#888b93] text-base bg-white/20 font-normal pr-6 pl-3 text-white" placeholder="شماره همراه" />
                 <button type="submit" @click.prevent="submitForm" class="flex_center max-w-64 w-full h-11 text-white bg-normal border border-white rounded-custom"> ارسال </button>
             </form>
         </section>
