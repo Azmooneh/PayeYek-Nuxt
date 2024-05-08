184338bd3146ed9182f4d7d4f93a9a1ff2bb6579
themestermim <themestermim@gmail.com>
2024-05-08 13:25:32 +0330

    some bugs fixed.

--- components/companyLanding/articles/children/children/article_type_5.vue
+++ components/companyLanding/articles/children/children/article_type_5.vue
@@ -3,12 +3,12 @@
         <Swiper class="w-full swiper articleSliderTypeFive" v-bind="settings" :modules="[SwiperPagination, SwiperAutoplay, SwiperNavigation]">
             <SwiperSlide v-for="(article, index) in articleList.slice(0, 8)" :key="index"
                          :class="'swiper-slide flex flex-col flex-none overflow-hidden rounded-custom ' + borderStyle + (evenOdd == 1 ? ' evenOdd_cards' : ' bg-white')">
-                <div class="relative w-full pt-[56%]">
+                <NuxtLink :to="'/l/' + slug + '/a/' + article.slug" class="relative w-full pt-[56%] block">
                     <NuxtImg :src="article.image" :alt="article.title"
                              class="absolute top-0 left-0 object-cover w-full h-full" width="100%" height="100%" format="webp" />
-                </div>
+                </NuxtLink>
                 <!-- info -->
-                <article class="px-2 pt-3 pb-4 text-stone-700">
+                <NuxtLink :to="'/l/' + slug + '/a/' + article.slug" class="px-2 pt-3 pb-4 text-stone-700">
                     <div class="gap-4 mb-1 flex_between">
                         <h3 class="text-base font-medium leading-7 sm:text-lg line-clamp-1">
                             {{ article.title }} </h3>
@@ -18,10 +18,9 @@
                         class="mb-2 text-sm font-normal leading-6 sm:leading-7 sm:h-20 sm:mb-3 text-justify line-clamp-3 h-[72px]">
                         {{ article.description }}
                     </p>
-                    <NuxtLink :to="'/l/' + slug + '/a/' + article.slug"
-                              class="w-32 h-10 mx-auto text-lg font-medium text-white bg-stone-700 flex_center rounded-custom">
-                        بیشتر </NuxtLink>
-                </article>
+                    <div class="w-32 h-10 mx-auto text-lg font-medium text-white bg-stone-700 flex_center rounded-custom">
+                        بیشتر </div>
+                </NuxtLink>
             </SwiperSlide>
         </Swiper>
     </section>
